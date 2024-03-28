2911a16e68fba8ec940c7bd715d2db407ef6dc68
Sharon Byegon <byegon.sharon@gmail.com>
2024-03-28 16:29:59 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+
 function addingEventListener() {
-}
+    //grabbing the element that we want to listen to
+    const input = document.getElementById('button');
+
+    //adding an event listener
+    input.addEventListener('click', () => {
+        alert("I was clicked!")} )
+    }
+   
+addingEventListener();
+
+
